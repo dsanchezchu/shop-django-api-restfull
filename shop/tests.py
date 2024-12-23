842b73fc72c5e7d270b1da6a9c2551a538d01d79
dsanchezchu <165108758+dsanchezchu@users.noreply.github.com>
2024-12-23 16:51:20 -0500

    feature(Test):  test de crear producto y categoria

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from shop.models import Category, Products
+# Realizar pruebas unitarias
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        Category.objects.create(name = "Test Category")
+        Products.objects.create(name='Test Products', description='Test Description', price=100, category=Category.objects.get(name='Test Category'))
+    
+    def test_product_category_id(self):
+        products=Products.objects.get(name='Test Products')
+        self.assertEqual(products.category.id,1)
 # Create your tests here.
